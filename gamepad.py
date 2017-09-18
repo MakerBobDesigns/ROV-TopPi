c3626f7379506766461193e23237f0ef4221bcbe
MakerBobDesigns <brainbounce@comcast.net>
2017-09-17 20:22:09 -0600

    First commit

---
+++ gamepad.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from evdev import InputDevice, categorize, ecodes, KeyEvent
+gamepad = InputDevice('/dev/input/event0')
+
+for event in gamepad.read_loop():
+    if event.type == ecodes.EV_KEY:
+        keyevent = categorize(event)
+        print keyevent.event.value
